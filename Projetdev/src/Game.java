b3beb65da6a4e5dec2139bf6c7039b2ac89b5106
Masterzengen <72359208+Masterzengen@users.noreply.github.com>
2021-05-11 16:15:02 +0200

    first

---
+++ Projetdev/src/Game.java
@@ -0,0 +1,23 @@
+import java.awt.Canvas;
+
+public class Game extends Canvas implements Runnable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1442798787354930462L;
+
+	public synchronized void start() {
+		
+	}
+	
+	public void run() {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
