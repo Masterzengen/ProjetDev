a83e7ad5ac634eb5ed01fd3a43e24862ddd80c9c
Masterzengen <72359208+Masterzengen@users.noreply.github.com>
2021-05-16 17:10:43 +0200

    bordure pour player

--- Projetdev/src/Player.java
+++ Projetdev/src/Player.java
@@ -8,10 +8,26 @@ public class Player extends GameObject {
 		//velX = 1;
 	}
 	
+	public static int limit(int x, int min, int max) {
+		if(x>=max) {
+			return x = max;
+		}else if(x<=min) {
+			return x=min;
+		}else {
+			return x;
+	}
+	}
+	
 	public void tick() { //chaque tick on update la position par la vélocité
 		x += velX;
 		y += velY;
+		
+		x = limit(x,0,Game.WIDTH - 46);
+		y = limit(y,0,Game.HEIGHT - 70);
+		
 	}
+	
+	
 
 	public void render(Graphics g) {
 		g.setColor(Color.white);
