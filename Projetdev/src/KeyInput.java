f9191b5e22d8ad8b78006d9f1e2fb3da1685a5de
Masterzengen <72359208+Masterzengen@users.noreply.github.com>
2021-05-16 16:38:02 +0200

    4 inputs fonctionnel

--- Projetdev/src/KeyInput.java
+++ Projetdev/src/KeyInput.java
@@ -19,9 +19,12 @@ public class KeyInput extends KeyAdapter {
 			
 			if(tempObject.getId() == ID.Player) {
 				//l'input n'affecte que l'objet Player.
-				if(key==KeyEvent.VK_W) {
-					tempObject.setY(tempObject.getY()-1);
-				}
+				if(key==KeyEvent.VK_Z) tempObject.setVelY(-3);
+				if(key==KeyEvent.VK_S) tempObject.setVelY(3);
+				if(key==KeyEvent.VK_D) tempObject.setVelX(3);
+				if(key==KeyEvent.VK_Q) tempObject.setVelX(-3);
+				
+				
 				
 			}
 					
@@ -31,6 +34,21 @@ public class KeyInput extends KeyAdapter {
 	
 	public void keyReleased(KeyEvent e) {
 		int key = e.getKeyCode();
+		
+		for(int i=0; i<handler.object.size(); i++) {
+			GameObject tempObject = handler.object.get(i);
+			
+			if(tempObject.getId() == ID.Player) {
+				//l'input n'affecte que l'objet Player.
+				if(key==KeyEvent.VK_Z) tempObject.setVelY(0);
+				if(key==KeyEvent.VK_S) tempObject.setVelY(0);
+				if(key==KeyEvent.VK_D) tempObject.setVelX(0);
+				if(key==KeyEvent.VK_Q) tempObject.setVelX(0);
+				
+				
+				
+			}
 	}
+}
 
 }
--- Projetdev/src/Player.java
+++ Projetdev/src/Player.java
@@ -5,16 +5,16 @@ public class Player extends GameObject {
 	
 	public Player(int x, int y, ID id) {
 		super(x,y,id);
-		velX = 1;
+		//velX = 1;
 	}
 	
 	public void tick() { //chaque tick on update la position par la vélocité
-		//x += velX;
-		//y += velY;
+		x += velX;
+		y += velY;
 	}
 
 	public void render(Graphics g) {
 		g.setColor(Color.white);
-		g.fillRect(x,y,32,32);
+		g.fillRoundRect(x,y,32,32,50,50);
 	}
 }
